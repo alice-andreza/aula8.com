0454d1544d7a187e7a518350f8bc0a2a9ed404c4
alice-andreza <92831218+alice-andreza@users.noreply.github.com>
2021-11-26 17:42:25 -0300

    Add files via upload

---
+++ Calculadora/script.js
@@ -0,0 +1,13 @@
+let num1= parseInt(prompt(`Digite um número.`))
+let operação= prompt(`Determine uma operação.`)
+let num2= parseInt(prompt(`Digite um número.`))
+
+
+if(operação === "+"){num1+num2
+alert (num1+num2)}
+else if(operação === "-"){num1-num2
+alert (num1-num2)}
+else if(operação === "*"){num1*num2
+alert(num1*num2)}
+else if (operação === "/"){num1/num2
+alert(num1/num2)}
